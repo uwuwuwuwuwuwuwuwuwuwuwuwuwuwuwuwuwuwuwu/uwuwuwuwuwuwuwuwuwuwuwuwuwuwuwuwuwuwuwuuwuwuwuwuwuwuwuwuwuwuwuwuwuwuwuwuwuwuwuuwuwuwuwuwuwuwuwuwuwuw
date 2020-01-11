707281cfd01cf62fbea9ee4f57d7ce7b778e70b7
Sami <devnilghosh2002@gmail.com>
2020-01-11 23:19:08 +0530

    Create uwu.c

---
+++ uwu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define u int
+#define w main
+#define u int
+#define uwu (
+#define uwuuwu )
+#define uwuuwuuwu {
+#define uwuuwuuwu }
+#define uwuuwuuwu putchar
+#define uwuuwuuwuuwuuwu 'uww'
+#define uwuuwuuwuuwuuwuuwuuwu 'uwu'
+#define uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu ;
+#define uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu while
+#define uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuu 1
+
+uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu
+uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu
+uwu
+uwu uwu uwu uwu uwu uwu uwu uwu uwu uwu 
+uwuuwuuwuuwu
