ee09377a5489807b92e039148432423ca101966e
Sami <devnilghosh2002@gmail.com>
2020-01-11 23:21:30 +0530

    Create uwu.cmd

---
+++ uwu.cmd
@@ -0,0 +1,6 @@
+@echo off
+TITLE uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu
+<nul set /p="uwu"
+:uwu
+<nul set /p="uwu"
+goto uwu
