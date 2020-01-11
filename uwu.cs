26267c20ae8c2c8ca8ebccd255e359b71c5405b4
Sami <devnilghosh2002@gmail.com>
2020-01-11 23:26:11 +0530

    Create uwu.cs

---
+++ uwu.cs
@@ -0,0 +1,12 @@
+using System;
+public class uwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwuuwu
+{
+    public static void Main() 
+    {
+        Console.Write('uwu');
+        while (true)
+        {
+            Console.Write('uwu');
+        }
+    }
+}
