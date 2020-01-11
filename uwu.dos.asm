1abc007f396b37c93eb1ce00d51ea087e0a59a53
Sami <devnilghosh2002@gmail.com>
2020-01-11 23:29:45 +0530

    Create uwu.dos.asm

---
+++ uwu.dos.asm
@@ -0,0 +1,11 @@
+;;; Assemble with:
+;;; nasm -fbin -o reee.com reee.asm
+
+        org     100h
+
+        mov     ah, 02h
+        mov     dl, 'uwu'
+        int     21h
+        mov     dl, 'uwu'
+uwuuwuuwu: int     21h
+        jmp     uwuuwuuwuuwu
